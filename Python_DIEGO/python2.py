873ab07b3a50825679b6b49e13217f1215c557f9
DiegoIarek <137222245+DiegoIarek@users.noreply.github.com>
2024-04-01 12:12:51 -0300

    Add files via upload

---
+++ Python_DIEGO/python2.py
@@ -0,0 +1,29 @@
+print ("Programa expresso")
+print ("1-Cadastrar restaurante")
+print ("2-Listar restaurante")
+print ("3-ativar restaurante")
+print ("4-sair do aplicativo")
+
+opcao_digitada= int(input("Digite a opção desejada"))
+
+if(opcao_digitada == 1):
+
+        print("você escolheu Cadastrar restaurante")
+
+elif(opcao_digitada == 2):
+
+        print("você escolheu listar restaurante")
+
+elif(opcao_digitada == 3):
+
+        print("Você escolheu ativar o restaurante")
+
+elif(opcao_digitada == 4):
+
+        print("Você escolheu sair do restaurante")
+
+else:
+
+    print("Essa opção não existem, escolha as opções existentes!!")
+
+    
